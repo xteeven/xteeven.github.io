64661abdc806618bfd901343c5ea51668bd2cb0b
xteeven <david7246@gmail.com>
2018-09-17 23:10:56 -0300

    Merge

--- blog/js/index.js
+++ blog/js/index.js
@@ -1,10 +1,28 @@
-$(".col-md-4").mouseover(function(){
-  $(this).find("h2").css("color","#000");
-  $(this).find(".read-more").removeClass("flyIn");
-  $(this).find(".read-more").addClass("fly");
-  
-}).mouseout(function(){
-  $("h2").css("color","#fff");
-  $(this).find(".read-more").removeClass("fly");
-  $(this).find(".read-more").addClass("flyIn");
-});
+$(document).on('ready',function(){
+  $(".col-md-4").mouseover(function(){
+    $(this).find("h2").css("color","#000");
+    $(this).find(".category").css("color","#000");
+    $(this).find(".read-more").removeClass("flyIn");
+    $(this).find(".read-more").addClass("fly");
+    
+  }).mouseout(function(){
+    $("h2").css("color","#fff");
+    $(this).find(".category").css("color","#fff");
+    $(this).find(".read-more").removeClass("fly");
+    $(this).find(".read-more").addClass("flyIn");
+
+
+    
+  });
+
+
+
+
+
+
+
+
+
+
+
+})
